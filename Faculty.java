814797ab90a1611cbe43aba870df4002cd0f29f7
unknown <penagalurih@gmail.com>
2023-06-30 10:59:13 +0530

    Student and Faculty

---
+++ Faculty.java
@@ -0,0 +1,10 @@
+public class Faculty {
+    public static void main(String[] args) {
+        System.out.println("List of Teachers:");
+        System.out.println("Dj");
+        System.out.println("Sivaiah");
+        System.out.println("Sudhakar");
+        System.out.println("sudheer");
+    }
+}
+
---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("List of Student in mca:");
+        System.out.println("Hari");
+        System.out.println("Amar");
+        System.out.println("Sai");
+        System.out.println("Ramesh");
+        System.out.println("Loki");
+    }
+}
